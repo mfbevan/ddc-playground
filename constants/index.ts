8aa47382917e3ff2b987e90545753a1629e0cd4a
mfbevan <matthewbevan00@gmail.com>
2023-08-10 14:47:39 +1000

    init: 🎉 new ddc playground project

---
+++ constants/index.ts
@@ -0,0 +1,1 @@
+export const MNEMONIC = process.env.MNEMONIC;
---
+++ scripts/ddc.ts
@@ -0,0 +1,10 @@
+import { MNEMONIC } from "../constants";
+
+const main = async () => {
+  console.log({ MNEMONIC });
+
+  console.log("Done");
+  process.exit(0);
+};
+
+main();
